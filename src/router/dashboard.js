e9ff1e96711cd54f52b06d00573813c24e8cc133
asher <asherjawaid99@gmail.com>
2024-09-28 19:24:34 +0500

    updated swagger

--- src/router/dashboard.js
+++ src/router/dashboard.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const { isAuthenticated, isAdmin } = require("../middleware/auth");
 const dashboard = require("../controllers/dashboardController");
 
-router.route("/").get(dashboard.dashboardStats);
-router.route("/income").get(dashboard.incomeStats);
-router.route("/orders").get(dashboard.orderStats);
+router.route("/").get(isAuthenticated, isAdmin, dashboard.dashboardStats);
+router.route("/income").get(isAuthenticated, isAdmin, dashboard.incomeStats);
+router.route("/orders").get(isAuthenticated, isAdmin, dashboard.orderStats);
 
 module.exports = router;
